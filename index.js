c3349d945e262e7fc0e6cdc7ed28cf9bc19b0d7c
Alexander Paschenko <alex@bringly.nl>
2024-04-28 18:47:23 +0300

    move dsn to .env file

--- index.js
+++ index.js
@@ -1,52 +1,48 @@
-// You can also use ESM `import * as Sentry from "@sentry/node"` instead of `require`
-import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { config } from 'dotenv';
+
+config();
+
+Sentry.init({
+  dsn: process.env.DSN,
+  integrations: [
+    Sentry.httpIntegration({ tracing: true, breadcrumbs: true }),
+
+    // I commented out this line, but nothing changed:
+    nodeProfilingIntegration(),
+    Sentry.expressIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
+
 import express from "express";
 
 const app = express();
 
-Sentry.init({
-  dsn: "https://9a372045661a2cea8e04c8826bc24f95@o990674.ingest.us.sentry.io/4507056912269312",
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Sentry.Integrations.Express({ app }),
-    nodeProfilingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
-
-// The request handler must be the first middleware on the app
-app.use(Sentry.Handlers.requestHandler());
-
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
-
-// All your controllers should live here
-app.get("/", function rootHandler(req, res) {
+app.get("/transaction", function rootHandler(req, res) {
   const message = 'It\'s a root!';
   console.log(message);
   Sentry.addBreadcrumb({ message });
   res.end("Hello world!");
 });
 
-// The error handler must be registered before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
+app.get("/error", function mainHandler(req, res) {
+  const message = 'It\'s a breadcrumb for exception!';
+  console.log(message);
+  Sentry.addBreadcrumb({ message });
+  throw new Error("My first Sentry error!");
+});
+
+Sentry.setupExpressErrorHandler(app);
 
 // Optional fallthrough error handler
 app.use(function onError(err, req, res, next) {
   // The error id is attached to `res.sentry` to be returned
   // and optionally displayed to the user for support.
   res.statusCode = 500;
-  res.end(res.sentry + "\n");
-});
-
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
+  res.end(`${res.sentry}\n`);
 });
 
 app.listen(3000);
